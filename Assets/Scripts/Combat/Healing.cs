4cfd5cc28b73139c7044416f4104d92355b1f375
imacpro <jack.33.wong@gmail.com>
2021-02-10 11:48:15 +0000

    healing

--- Assets/Scripts/Combat/Healing.cs
+++ Assets/Scripts/Combat/Healing.cs
@@ -8,6 +8,9 @@ public class Healing : MonoBehaviour
 {
     private TacticalBehavior tb;
     private RTSPlayer player;
+    private GameObject capsule;
+    private int healingRange = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +21,16 @@ public class Healing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(tb.GetBehaviorSelectionType() == TacticalBehavior.BehaviorSelectionType.Defend)
+
+        if (tb.GetBehaviorSelectionType() != TacticalBehavior.BehaviorSelectionType.Defend) { return; }
+        
+        GameObject[] armies = GameObject.FindGameObjectsWithTag("Player" + player.GetPlayerID());
+        capsule = GameObject.FindGameObjectWithTag("PlayerBase" + player.GetPlayerID());
+        foreach (GameObject army in armies)
         {
-            GameObject[] armies = GameObject.FindGameObjectsWithTag("Player" + player.GetPlayerID());
-            foreach (GameObject army in armies)
-            {
+            if ((capsule.transform.position - army.transform.position).sqrMagnitude < healingRange * healingRange)
                 army.GetComponent<Health>().Healing(Health.healingSpeed);
-            }
         }
+        
     }
 }
