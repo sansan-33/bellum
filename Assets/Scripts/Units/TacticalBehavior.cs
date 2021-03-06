235aa016dbcfa001fada5cc35d09ea6a069e3653
imacpro <jack.33.wong@gmail.com>
2021-03-06 12:27:41 +0000

    unit tactical

--- Assets/Scripts/Units/TacticalBehavior.cs
+++ Assets/Scripts/Units/TacticalBehavior.cs
@@ -276,12 +276,12 @@ public class TacticalBehavior : MonoBehaviour
     public void TryReinforcePlayer(Unit unit)
     {
         //if (unit.tag == ENEMYTAG) { return; }
-        //Debug.Log($"Auto Reinforce ..... {unit.name}");
+        Debug.Log($"Auto Reinforce ..... {unit.name}");
         StartCoroutine(TacticalFormation(PLAYERID, ENEMYID));
     }
     public void TryUpdateLeader(Unit unit)
     {
-        if (unit.tag == ENEMYTAG) { return; }
+        //if (unit.tag == ENEMYTAG) { return; }
         if (!unit.name.ToLower().Contains("leader")) { return; }
         //Debug.Log($"Auto TryUpdateLeader ..... {unit.name} {unit.unitType} is killed !!!");
         StartCoroutine(TacticalFormation(PLAYERID, ENEMYID));
