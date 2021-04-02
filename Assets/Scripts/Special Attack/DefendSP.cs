bff1f9adb251ef13b0d013e4765a32ccd88c9a8f
Leonard Wong <leonard.11.wong@gmail.com>
2021-04-02 14:45:17 +0100

    refactor

--- Assets/Scripts/Special Attack/DefendSP.cs
+++ Assets/Scripts/Special Attack/DefendSP.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,8 +11,10 @@ public class DefendSP : MonoBehaviour
     public float SPCost = 10;
     public int shieldHealths = 100;
     private Button SPButton;
+    private RTSPlayer player;
     void Start()
     {
+        player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
         spCost = FindObjectOfType<SpCost>();
         SPButton = GameObject.FindGameObjectWithTag("SpDefend").GetComponent<Button>();
         SPButton.onClick.RemoveAllListeners();
@@ -22,20 +25,85 @@ public class DefendSP : MonoBehaviour
     {
         //if(SPAmount < SPCost) {return;}
         spCost.SPAmount -= (int)SPCost;
-        GameObject[] shieldList;
-        shieldList = GameObject.FindGameObjectsWithTag("Shield");
-        foreach (GameObject shield in shieldList)
-        {
-            if (shield.transform.parent.CompareTag("Player0")|| shield.transform.parent.CompareTag("King0"))
-            {
-                shield.GetComponent<Shield>().shieldHealth = shieldHealths;
+        Unit[] shieldList;
+        shieldList = FindObjectsOfType<Unit>();
 
+        if (((RTSNetworkManager)NetworkManager.singleton).Players.Count == 1)
+            {
+                foreach (Unit shield in shieldList)
+                {
+                    if (shield.CompareTag("Player0") || shield.CompareTag("King0"))
+                    {
+                      shield.GetComponent<Shield>().CmdSetShieldHealth(shieldHealths);
+                    }
+                }
             }
+            else // Multi player seneriao
+            {
+                //Debug.Log($"OnPointerDown Defend SP Multi shieldList {shieldList.Length}");
+                foreach (Unit shield in shieldList)
+                {
+                    //Debug.Log($"shield tag {shield.tag}");
+
+                    if (shield.CompareTag("Player" + player.GetPlayerID()) || shield.CompareTag("King" + player.GetPlayerID()))
+                    {
+                        //shield.GetComponent<Shield>().shieldHealth = shieldHealths;
+                        //Debug.Log($"player {player.GetPlayerID()} , set shield health  {shield.tag} / {shieldHealths}");
+
+                        shield.GetComponent<Shield>().CmdSetShieldHealth(shieldHealths);
+                        //CommandShield(shield, shieldHealths);
+                    }
+                }
+            /*
+                if (player.GetPlayerID() == 0)
+                {
+                  foreach (GameObject shield in shieldList)
+                  {
+                    if (shield.transform.parent.CompareTag("Player0") || shield.transform.parent.CompareTag("King0"))
+                    {
+                       
+                        shield.GetComponent<Shield>().shieldHealth = shieldHealths;
+                        CommandShield(shield, shieldHealths);
+                    }
+                   }
+                }
+                else
+                {
+                  foreach (GameObject shield in shieldList)
+                  {
+                    if (shield.transform.parent.CompareTag("Player1") || shield.transform.parent.CompareTag("King1"))
+                    {
+                        
+                        shield.GetComponent<Shield>().shieldHealth = shieldHealths;
+
+                        CommandShield(shield, shieldHealths);
+                    }
+                  }
+                }
+            */
         }
+        
+    }
+    /*
+    [Command]
+    void CommandShield(GameObject shield, int shieldHealths)
+    {
+        ServerShield(shield, shieldHealths);
+    }
+    [Server]
+    void ServerShield(GameObject shield, int shieldHealths)
+    {
+        shield.GetComponent<Shield>().shieldHealth = shieldHealths;
+    }
+    [ClientRpc]
+    void RpcShield(GameObject shield,int shieldHealths)
+    {       
+       
     }
     // Update is called once per frame
     void Update()
     {
         
     }
+    */
 }
--- Assets/Scripts/Special Attack/Shield.cs
+++ Assets/Scripts/Special Attack/Shield.cs
@@ -1,22 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class Shield : MonoBehaviour
+public class Shield : NetworkBehaviour
 {
     [SerializeField] private ParticleSystem ShieldEffect;
+    private ParticleSystem.MainModule ShieldEffectMain;
+    private ParticleSystem ShieldEffects;
+    private List<ParticleSystem> ShieldEffectList = new List<ParticleSystem>();
+    [SyncVar]
     public float shieldHealth = 0;
     void Start()
     {
         
     }
+    [Command]
+    public void CmdSetShieldHealth(int shieldHealth)
+    {
+        this.shieldHealth = shieldHealth;
+        //ebug.Log($"gameobject {this.gameObject.name} {this.shieldHealth} / {shieldHealth}");
+        for (int i = 10; i > 0; i--)
+        {
+            ShieldEffects = Instantiate(ShieldEffect, transform);
+            ShieldEffects.transform.localScale = new Vector3(5, 5, 5);
+            ShieldEffectMain = ShieldEffects.main;
+            ShieldEffects.Pause();
+            ShieldEffectList.Add(ShieldEffects);
+        }
+       
 
+        Debug.Log(ShieldEffects);
+    }
     // Update is called once per frame
     void Update()
     {
-        if(shieldHealth > 0)
+        if(shieldHealth <= 0)
         {
-            Instantiate(ShieldEffect, this.transform).transform.localScale = new Vector3(5, 5, 5);
+           foreach(ParticleSystem Shied in ShieldEffectList)
+            {
+                Destroy(Shied);
+            }
         }
     }
 }
