89e8cdb4a23f23a7636da97eb1d2344fd969ceb0
imacpro <jack.33.wong@gmail.com>
2021-05-16 16:03:24 +0100

    new stage

---
+++ Assets/Scripts/Menus/StageMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject lobbyUI = null;
+    [SerializeField] private Button startGameButton = null;
+    
+    private void Start()
+    {
+        RTSNetworkManager.ClientOnConnected += HandleClientConnected;
+        RTSPlayer.AuthorityOnPartyOwnerStateUpdated += AuthorityHandlePartyOwnerStateUpdated;
+        RTSPlayer.ClientOnInfoUpdated += ClientHandleInfoUpdated;
+    }
+
+    private void OnDestroy()
+    {
+        RTSNetworkManager.ClientOnConnected -= HandleClientConnected;
+        RTSPlayer.AuthorityOnPartyOwnerStateUpdated -= AuthorityHandlePartyOwnerStateUpdated;
+        RTSPlayer.ClientOnInfoUpdated -= ClientHandleInfoUpdated;
+    }
+
+    private void HandleClientConnected()
+    {
+        lobbyUI.SetActive(true);
+    }
+
+    private void ClientHandleInfoUpdated()
+    {
+        NetworkClient.connection.identity.GetComponent<RTSPlayer>().CmdSetUserID(StaticClass.UserID);
+        List<RTSPlayer> players = ((RTSNetworkManager)NetworkManager.singleton).Players;
+        for (int i = 0; i < players.Count; i++)
+        {
+        }
+    }
+
+    private void AuthorityHandlePartyOwnerStateUpdated(bool state)
+    {
+        startGameButton.gameObject.SetActive(state);
+    }
+
+    public void StartGame()
+    {
+        NetworkClient.connection.identity.GetComponent<RTSPlayer>().CmdStartGame();
+    }
+
+    public void LeaveLobby()
+    {
+
+        Debug.Log("LeaveLobby");
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            NetworkManager.singleton.StopHost();
+        }
+        else
+        {
+            NetworkManager.singleton.StopClient();
+
+            Debug.Log("LeaveLobby Scene_Main_Menu");
+            SceneManager.LoadScene("Scene_Main_Menu");
+        }
+    }
+}
