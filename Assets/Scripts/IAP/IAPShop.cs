642c196aec9f19fd27413cc6cba4cfa4c5c72fa4
Anthea Yip <antheayip@gmail.com>
2021-05-20 16:57:22 +0100

    payment

---
+++ Assets/Scripts/IAP/IAPShop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class IAPShop : MonoBehaviour
+{
+    private string buy = "com.sansan-33.bellum.buy";
+
+    public void OnPurchaseComplete(Product product)
+    {
+        if (product.definition.id == "buy")
+        {
+            // implement buy action
+            Debug.Log("Buy Success! Get gem!!!");
+        }
+
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
+    {
+
+    }
+}
