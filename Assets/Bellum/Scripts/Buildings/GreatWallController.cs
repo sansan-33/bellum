257b7b508dc909d16bcea8c4e7d90139198cd5cf
imacpro <jack.33.wong@gmail.com>
2021-06-27 12:22:41 +0100

    map 01

--- Assets/Bellum/Scripts/Buildings/GreatWallController.cs
+++ Assets/Bellum/Scripts/Buildings/GreatWallController.cs
@@ -3,33 +3,33 @@ using Mirror;
 
 public class GreatWallController : NetworkBehaviour
 {
-    public static event Action<string> GateOpened;
+    public static event Action<string, string> GateOpened;
 
     //==================================== Set Skill For Unit
-    public void GateOpen(string playerid)
+    public void GateOpen(string playerid, string doorIndex)
     {
         if (isServer)
-            RpcGateOpen(playerid);
+            RpcGateOpen(playerid, doorIndex);
         else
-            CmdGateOpen(playerid);
+            CmdGateOpen(playerid, doorIndex);
     }
     [Command]
-    public void CmdGateOpen(string playerid)
+    public void CmdGateOpen(string playerid, string doorIndex)
     {
-        ServerGateOpen(playerid);
+        ServerGateOpen(playerid, doorIndex);
     }
     [ClientRpc]
-    public void RpcGateOpen(string playerid)
+    public void RpcGateOpen(string playerid, string doorIndex)
     {
-        HandleGateOpen(playerid);
+        HandleGateOpen(playerid, doorIndex);
     }
     [Server]
-    public void ServerGateOpen(string playerid)
+    public void ServerGateOpen(string playerid, string doorIndex)
     {
-        HandleGateOpen(playerid);
+        HandleGateOpen(playerid, doorIndex);
     }
-    private void HandleGateOpen(string playerid)
+    private void HandleGateOpen(string playerid, string doorIndex)
     {
-        GateOpened?.Invoke("" + playerid);
+        GateOpened?.Invoke("" + playerid, doorIndex);
     }
 }
