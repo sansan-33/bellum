efc853dc8fea2b5f5da59aa77e086719bad5b865
Leonard Wong <leonard.11.wong@gmail.com>
2021-07-03 11:30:01 +0100

    o click effect on stage ,menu

---
+++ Assets/Bellum/Scripts/Utility/OnclickEffect.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class OnclickEffect : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    [SerializeField] private LayerMask floorMask = new LayerMask();
+    [SerializeField] GameObject effect;
+    [SerializeField] Transform parent;
+    Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("ON click");
+        Vector3 pos = Input.touchCount > 0 ? Input.GetTouch(0).position : Mouse.current.position.ReadValue();
+        Debug.Log($"spawn pos{pos}");
+        GameObject _effct = Instantiate(effect, parent);
+        _effct.transform.position = pos;
+    }
+    public void OnPointerClick()
+    {
+        Debug.Log("ON click");
+        Vector3 pos = Input.touchCount > 0 ? Input.GetTouch(0).position : Mouse.current.position.ReadValue();
+        Debug.Log($"spawn pos{pos}");
+        GameObject _effct = Instantiate(effect, parent);
+        _effct.transform.position = pos;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
