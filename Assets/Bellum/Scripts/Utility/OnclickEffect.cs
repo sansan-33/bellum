68f7e0148e16330b86cbcc31692f0c3287e3fbaa
Leonard Wong <leonard.11.wong@gmail.com>
2021-07-04 11:16:30 +0100

    on click effect on main menu

--- Assets/Bellum/Scripts/Utility/OnclickEffect.cs
+++ Assets/Bellum/Scripts/Utility/OnclickEffect.cs
@@ -22,7 +22,7 @@ public class OnclickEffect : MonoBehaviour, IPointerClickHandler
         Vector3 pos = Input.touchCount > 0 ? Input.GetTouch(0).position : Mouse.current.position.ReadValue();
         Debug.Log($"spawn pos{pos}");
         _effect = Instantiate(effect, parent);
-        _effect.GetComponent<RectTransform>().SetAnchor(AnchorPresets.MiddleCenter);
+        //_effect.GetComponent<RectTransform>().SetAnchor(AnchorPresets.MiddleCenter);
         _effect.transform.position = pos;
         _effect.GetComponent<ParticleSystem>().Play();
         
@@ -34,7 +34,7 @@ public class OnclickEffect : MonoBehaviour, IPointerClickHandler
         Vector3 pos = Input.touchCount > 0 ? Input.GetTouch(0).position : Mouse.current.position.ReadValue();
         Debug.Log($"spawn pos{pos}");
         _effect = Instantiate(effect, parent);
-        _effect.GetComponent<RectTransform>().SetAnchor(AnchorPresets.MiddleCenter);
+        //_effect.GetComponent<RectTransform>().SetAnchor(AnchorPresets.MiddleCenter);
         _effect.transform.position = pos;
         _effect.GetComponent<ParticleSystem>().Play();
         
@@ -43,7 +43,7 @@ public class OnclickEffect : MonoBehaviour, IPointerClickHandler
     private IEnumerator DisableEffect(GameObject effect)
     {
         yield return new WaitForSeconds(0.4f);
-        //Destroy(effect);
+        Destroy(effect);
     }
     // Update is called once per frame
     void Update()
